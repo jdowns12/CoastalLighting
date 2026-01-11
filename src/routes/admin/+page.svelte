84461bf00cdbe2a2c9023e3a40346c0705b47fda
jdowns12 <jadon.downs@gmail.com>
2026-01-11 03:17:09 -0600

    Add admin redirect route

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { onMount } from 'svelte';
+  import { browser } from '$app/environment';
+  
+  onMount(() => {
+    if (browser) {
+      window.location.href = '/admin/index.html';
+    }
+  });
+</script>
+
+<p>Redirecting to admin...</p>
